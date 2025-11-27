b1158204a9402add72cd2621a3cce6431a8f70ef
Edward Gereis <eg628@live.mdx.ac.uk>
2025-11-27 15:12:19 +0000

    new seeded images

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ import express from "express";
 import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use("/lesson-images", express.static(path.join(__dirname, "images")));
 
 const client = new MongoClient(process.env.MONGO_URI);
 let lessonsCollection;
@@ -41,28 +47,28 @@ app.get("/seed-lessons", async (req, res) => {
       location: "Hendon",
       price: 100,
       spaces: 5,
-      image: "https://images.unsplash.com/photo-1509223197845-458d87318791",
+      image: "mathsimage.png",
     },
     {
       subject: "English",
       location: "Colindale",
       price: 90,
       spaces: 5,
-      image: "https://images.unsplash.com/photo-1498075702571-ecb018f3752d",
+      image: "english_image.png",
     },
     {
       subject: "Science",
       location: "Brent Cross",
       price: 110,
       spaces: 5,
-      image: "https://images.unsplash.com/photo-1518770660439-4636190af475",
+      image: "science_image.png",
     },
     {
       subject: "Coding",
       location: "Edgware",
       price: 120,
       spaces: 5,
-      image: "https://images.unsplash.com/photo-1518779578993-ec3579fee39f",
+      image: "coding_image.png",
     },
   ];
 
